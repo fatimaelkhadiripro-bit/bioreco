2b63bef76187d132dd73189dae88a02be8ae736e
FatimaElkhadiri <105443759+fatimaelkhadiri@users.noreply.github.com>
2026-08-06 23:23:09 +0200

    fix: update base URL and bypass jekyll

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures relative assets work on GitHub Pages out-of-the-box
+  base: '/bioreco/', // Exact base URL for GitHub Pages repository
 })
